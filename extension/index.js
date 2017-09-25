1baf15326c368267b0da017a5f5a4b1f274e18a1
Hugo Kiiski <hugo.kiiski@hotmail.com>
2017-09-26 00:29:57 +0300

    Basic UI

--- extension/index.js
+++ extension/index.js
@@ -1,54 +1,75 @@
-const insertAddButton = () => {
-  const addBtn = createAddButton();
-  if (!addBtn) {
-    // Description hasn't loaded yet, wait
-    return setTimeout(insertAddButton, 100);
-  }
-  addBtn.addEventListener("click", showAddToPlaylistMenu);
-};
+const ROOT_URL = "http://localhost:5000";
 
-const showAddToPlaylistMenu = () => {
-  const html = `<div class="yt2gpm menu">Text</div>`;
-  const parser = new DOMParser();
-  const doc = parser.parseFromString(html, "text/xml");
-  const app = document.querySelector("body");
-  app.appendChild(doc.firstChild);
-};
-
-const createAddButton = () => {
-  const ytAddBtn = document.querySelector(`button[aria-label="Add to"]`);
-  if (!ytAddBtn) return;
-  // Deep clone doesn't work for some reason, clone manually and append
-  const childClone = ytAddBtn.parentNode.cloneNode(true);
-  const parentClone = ytAddBtn.parentNode.parentNode.cloneNode(true);
-  parentClone.appendChild(childClone);
-  parentClone.title = "Add to Google Play Music";
-
-  const btnRow = ytAddBtn.parentNode.parentNode.parentNode;
-  btnRow.appendChild(parentClone);
-  return parentClone;
-};
-
-let playlistCache = [];
 const fetchPlaylists = () => {
-  if (playlistCache.length > 0) return Promise.resolve(playlistCache);
-  return sendMessage("FETCH_PLAYLISTS")
+  return fetch(`${ROOT_URL}/playlists`)
+    .then(response => response.json())
+    .then(json => json.playlists);
+};
+
+const navigateToPlaylists = () => {
+  renderPlaylists();
+};
+
+const navigateToAddSong = selectedPlaylist => {
+  renderAddSongForm();
+};
+
+const renderPlaylists = () => {
+  const header = document.querySelector(".header h1");
+  header.textContent = "Select a playlist";
+  const content = document.querySelector(".content");
+  Array.from(content.children).forEach(c => content.removeChild(c));
+  fetchPlaylists()
     .then(playlists => {
-      playlistCache = playlists;
-      return playlists;
+      playlists.forEach(playlist => {
+        const elem = buildPlaylistElement(playlist);
+        elem.addEventListener("click", navigateToAddSong);
+        content.appendChild(elem);
+      });
     });
 };
 
-const sendMessage = (type, payload) => {
-  console.log("[Send]", type, payload);
-  return new Promise((resolve, reject) => {
-    chrome.extension.sendMessage({ type, payload }, response => {
-      console.log("[Receive]", response);
-      if (!response) return reject(new Error("Received empty response from background script"));
-      if (response.error) return reject(new Error(response.error));
-      resolve(response.payload);
-    });
-  });
+const renderAddSongForm = () => {
+  const header = document.querySelector(".header h1");
+  header.textContent = "Edit song";
+  const content = document.querySelector(".content");
+  Array.from(content.children).forEach(c => content.removeChild(c));
+  content.appendChild(buildAddSongFormElement());
+  const buttonContainer = parseHTML(`<div class="action-btn-container"></div>`);
+  const addToPlaylistBtn = buildActionButtonElement("Add to playlist");
+  const backToPlaylistsBtn = buildActionButtonElement("Back to playlists");
+
+  backToPlaylistsBtn.addEventListener("click", navigateToPlaylists);
+
+  buttonContainer.appendChild(addToPlaylistBtn);
+  buttonContainer.appendChild(backToPlaylistsBtn);
+  content.appendChild(buttonContainer);
 };
 
-insertAddButton();
+const buildPlaylistElement = playlist => {
+  const html = `<div class="playlist">
+                  <h2>${playlist.name}</h2>
+                </div>`;
+  return parseHTML(html);
+};
+
+const buildAddSongFormElement = () => {
+  const html = `<form>
+                  <input type="text" placeholder="Artist"/>
+                  <input type="text" placeholder="Title"/>
+                </form>`;
+  return parseHTML(html);
+};
+
+const buildActionButtonElement = (text, className) => {
+  const html = `<button class="action">
+                  ${text}
+                </button>`;
+  return parseHTML(html);
+}
+ 
+const parseHTML = htmlString => {
+  const parser = new DOMParser();
+  return parser.parseFromString(htmlString, "text/html").body.firstChild;
+};
+navigateToPlaylists();
