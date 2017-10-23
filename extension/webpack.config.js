f42c54623181d00038805129c90b4bb2080ad7c1
Hugo Kiiski <hugo.kiiski@hotmail.com>
2017-10-23 21:38:33 +0300

    Very important improvements to loading spinner

--- extension/webpack.config.js
+++ extension/webpack.config.js
@@ -8,7 +8,7 @@ module.exports = {
   },
   module: {
     loaders: [
-      { test: /\.jsx?$/, loader: "babel-loader", exclude: /node_modules/, query: { presets: [ "es2015", "react" ]}}
+      { test: /\.jsx?$/, loader: "babel-loader", exclude: /node_modules/, query: { presets: [ "es2017", "react" ]}}
     ]
   }
 };
